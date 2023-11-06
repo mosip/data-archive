c1616056b12c35a2fda5e72fa0baef5fa3dc69ad
bhumi46 <111699703+bhumi46@users.noreply.github.com>
2023-11-06 17:23:40 +0530

    Update entrypoint.sh
    Signed-off-by: bhumi46 <111699703+bhumi46@users.noreply.github.com>

--- data-archive/entrypoint.sh
+++ data-archive/entrypoint.sh
@@ -7,7 +7,7 @@ set -e
 #cd db_scripts/mosip_archive
 
 # Execute the deployment script without the properties file..,as it was passed by arguments
-bash deploy.sh
+#bash deploy.sh
 
 #echo "executed db_scripts succesfully"
 
