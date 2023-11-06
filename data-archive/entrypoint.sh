a6e871bc892794896912f3386acd09e5ee917896
bhumi46 <thisisbn46@gmail.com>
2023-11-06 18:51:27 +0530

    mosip
    Signed-off-by: bhumi46 <thisisbn46@gmail.com>

--- data-archive/entrypoint.sh
+++ data-archive/entrypoint.sh
@@ -2,7 +2,6 @@
 # entrypoint.sh
 set -e
 
-<<<<<<< HEAD
 #echo "Executing db.sh"
 
 #bash db.sh
@@ -13,17 +12,5 @@ set -e
 echo "Executing archive.sh"
 
 bash archive.sh
-=======
-#echo "Executing db_scripts"
-
-#cd db_scripts/mosip_archive
-
-# Execute the deployment script without the properties file..,as it was passed by arguments
-#bash deploy.sh
-
-#echo "executed db_scripts succesfully"
-
-echo "Executing archive-jobs sequentilay which was mentioned in DB_NAMES"
->>>>>>> c1616056b12c35a2fda5e72fa0baef5fa3dc69ad
 
 echo "executed archive.sh successfully"
