91b25f4e44c9dce72ad58184a04b737c9c0b34b1
bhumi46 <111699703+bhumi46@users.noreply.github.com>
2024-09-23 20:29:39 +0530

    Update mosip_archive_main.py
    Signed-off-by: bhumi46 <111699703+bhumi46@users.noreply.github.com>

--- data-archive/archive-jobs/mosip_archive_main.py
+++ data-archive/archive-jobs/mosip_archive_main.py
@@ -9,6 +9,7 @@ import configparser
 import json
 from datetime import datetime
 from psycopg2 import extras
+from concurrent.futures import ThreadPoolExecutor, as_completed 
 
 # Define batch size
 #BATCH_SIZE = 10
