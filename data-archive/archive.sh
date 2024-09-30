1b3a9ff55bfa21860281ec7f0b2c90660b81d566
bhumi46 <111699703+bhumi46@users.noreply.github.com>
2024-09-30 10:50:50 +0530

    Update archive.sh
    Signed-off-by: bhumi46 <111699703+bhumi46@users.noreply.github.com>

--- data-archive/archive.sh
+++ data-archive/archive.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-echo "Executing archive-jobs sequentilay"
+# Enable strict error handling
+set -euo pipefail
 
+echo "Executing archive-jobs sequentially"
+
+# Navigate to the archive-jobs directory
 cd archive-jobs
 
+# Execute the Python script
 python3 mosip_archive_main.py
 
 echo "Executed archive-jobs successfully"
+
